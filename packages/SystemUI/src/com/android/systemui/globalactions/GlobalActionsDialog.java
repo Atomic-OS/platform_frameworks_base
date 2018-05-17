328cd9ff866624391007b6f51a9bb72fa06e0b1a
Alex <alex@dirtyunicorns.com>
2018-05-17 02:41:38 -0500

    Multi-User fixes
    This change allows for the features listed below to work while using
    a guest account (multi-user)
    
    - Power menu options
    - 4g/lte network switch
    - Navigation bar QS tile
    
    Change-Id: I46d15a42e8e05702c85d0aea52e79617a28a681a
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -355,8 +355,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             }
 
             public boolean showDuringKeyguard() {
-                boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_LS_AIRPLANE, 0) == 1;
+                boolean showlocked = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_LS_AIRPLANE, 0, UserHandle.USER_CURRENT) == 1;
                 return showlocked;
             }
 
@@ -377,8 +377,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
                 mWindowManagerFuncs, mHandler) {
 
             public boolean showDuringKeyguard() {
-                boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 0) == 1;
+                boolean showlocked = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_LS_ADVANCED_REBOOT, 0, UserHandle.USER_CURRENT) == 1;
                 return showlocked;
             }
 
@@ -461,8 +461,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             if (GLOBAL_ACTION_KEY_POWER.equals(actionKey)) {
                 mItems.add(new PowerAction());
             } else if (GLOBAL_ACTION_KEY_AIRPLANE.equals(actionKey)) {
-                if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_AIRPLANE, 0) != 0 && !isInLockTaskMode()) {
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_AIRPLANE, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(mAirplaneModeOn);
                 }
             } else if (GLOBAL_ACTION_KEY_BUGREPORT.equals(actionKey)) {
@@ -487,19 +487,19 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
             } else if (GLOBAL_ACTION_KEY_ASSIST.equals(actionKey)) {
                 //mItems.add(getAssistAction());
             } else if (GLOBAL_ACTION_KEY_RESTART.equals(actionKey)) {
-                if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_REBOOT, 1) == 1) {
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_REBOOT, 1, UserHandle.USER_CURRENT) == 1) {
                     mItems.add(new RestartAction());
                 }
             } else if (GLOBAL_ACTION_KEY_ADVANCED.equals(actionKey)) {
-                if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_REBOOT, 1) == 1 && Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_ADVANCED_REBOOT, 0) != 0 && !isInLockTaskMode()) {
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_REBOOT, 1, UserHandle.USER_CURRENT) == 1 && Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_ADVANCED_REBOOT, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(mShowAdvancedToggles);
                 }
             } else if (GLOBAL_ACTION_KEY_SCREENSHOT.equals(actionKey)) {
-                if (Settings.System.getInt(mContext.getContentResolver(),
-                        Settings.System.POWERMENU_SCREENSHOT, 0) != 0 && !isInLockTaskMode()) {
+                if (Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.POWERMENU_SCREENSHOT, 0, UserHandle.USER_CURRENT) != 0 && !isInLockTaskMode()) {
                     mItems.add(new ScreenshotAction());
                 }
             } else {
@@ -587,8 +587,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public boolean showDuringKeyguard() {
-            boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.POWERMENU_LS_REBOOT, 1) == 1;
+            boolean showlocked = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.POWERMENU_LS_REBOOT, 1, UserHandle.USER_CURRENT) == 1;
             return showlocked;
         }
 
@@ -625,8 +625,8 @@ class GlobalActionsDialog implements DialogInterface.OnDismissListener, DialogIn
 
         @Override
         public boolean showDuringKeyguard() {
-            boolean showlocked = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.POWERMENU_LS_SCREENSHOT, 0) == 1;
+            boolean showlocked = Settings.System.getIntForUser(mContext.getContentResolver(),
+                    Settings.System.POWERMENU_LS_SCREENSHOT, 0, UserHandle.USER_CURRENT) == 1;
             return showlocked;
         }
 
